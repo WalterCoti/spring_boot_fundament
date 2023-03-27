248178715076eb467c7b8b072e1d742fd0a804cd
Walter Coti <3064960620401@ingenieria.usac.edu.gt>
2023-03-27 00:53:33 -0600

    new changes

--- src/main/java/com/practicaSpringBoot/demo/DemoApplication.java
+++ src/main/java/com/practicaSpringBoot/demo/DemoApplication.java
@@ -5,12 +5,15 @@ import com.practicaSpringBoot.demo.bean.MybeanDependency;
 import com.practicaSpringBoot.demo.bean.MybeanPropertiesImplement;
 import com.practicaSpringBoot.demo.bean.newbean;
 import com.practicaSpringBoot.demo.component.ComponentDependency;
+import com.practicaSpringBoot.demo.entity.User;
 import com.practicaSpringBoot.demo.pojo.UserPojo;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 
@@ -42,9 +45,29 @@ public class DemoApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	 componentDependency.saludar();
-	 mybean.print();
-	 mybeanwithDependency.printmyDependency();
+		ejemplosanteriores();
+
+	}
+
+	private void savUserInDataBase(){
+		User user1 = new User("Jonh","holamundo@gmail.com", LocalDate.of(2023,03,27));
+		User user2 = new User("Antuan","Antuan@gmail.com", LocalDate.of(2023,04,27));
+		User user3 = new User("Miguel","Miguel@gmail.com", LocalDate.of(2023,04,27));
+		User user4 = new User("Jose","Jose@gmail.com", LocalDate.of(2023,04,27));
+		User user5 = new User("Mary","Mary@gmail.com", LocalDate.of(2023,04,27));
+		User user6 = new User("Letycia","Letycia@gmail.com", LocalDate.of(2023,04,27));
+		User user7 = new User("Romio","Romio@gmail.com", LocalDate.of(2023,04,27));
+		User user8 = new User("Manuela","Manuela@gmail.com", LocalDate.of(2023,04,27));
+		User user9 = new User("Fernanda","Fernanda@gmail.com", LocalDate.of(2023,04,27));
+		User user10 = new User("Estefany","Estefany@gmail.com", LocalDate.of(2023,04,27));
+
+
+	}
+
+	public void ejemplosanteriores(){
+		componentDependency.saludar();
+		mybean.print();
+		mybeanwithDependency.printmyDependency();
 		System.out.println(myBeanwithProperties.function());
 		System.out.println(userPojo.toString());
 		try{
@@ -52,7 +75,7 @@ public class DemoApplication  implements CommandLineRunner {
 			LOGGER.debug("Mi valor : " + value);
 		}catch (Exception e){
 
-		LOGGER.error("esto es un error del aplicativo " + e.getMessage());
+			LOGGER.error("esto es un error del aplicativo " + e.getMessage());
 		}
 	}
 }
---
+++ src/main/java/com/practicaSpringBoot/demo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.practicaSpringBoot.demo.repository;
+
+import com.practicaSpringBoot.demo.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+
+}
