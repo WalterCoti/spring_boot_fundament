85c4643f5a2fb56e1fe2ac06b2348e6245fb75b3
Walter Cotí <wgustavocoti@gmail.com>
2023-03-27 00:27:23 -0600

    configuracion datasource  a nivel de properties & clases

--- src/main/java/com/practicaSpringBoot/demo/configuration/Generalconfiguaration.java
+++ src/main/java/com/practicaSpringBoot/demo/configuration/Generalconfiguaration.java
@@ -6,8 +6,12 @@ import com.practicaSpringBoot.demo.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
 public class Generalconfiguaration {
@@ -24,6 +28,16 @@ public class Generalconfiguaration {
     public MyBeanwithProperties function(){
         return  new MybeanPropertiesImplement(name, apellido);
 
+    }
+//configuracion datasource a nivel de clase
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 
 }
